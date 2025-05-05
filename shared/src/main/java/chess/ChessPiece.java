3175431319d3c662548b81339c72f145fdff7d08
Jacob Larson <jl2379@byu.edu>
2025-05-04 20:13:20 -0600

    Completed getKingMoves function in ChessPiece

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -3,6 +3,7 @@ package chess;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Objects;
+import java.util.Vector;
 
 /**
  * Represents a single chess piece
@@ -93,7 +94,50 @@ public class ChessPiece {
 
     private Collection<ChessMove> getKingMoves(ChessBoard board, ChessPosition myPosition) {
         Collection<ChessMove> kingMoves = new ArrayList<>();
+        int myRow = myPosition.getRow();
+        int myCol = myPosition.getColumn();
+        ChessGame.TeamColor myColor = getTeamColor();
+
+        //populates possibleMoves with every square around and including myPosition
+        Vector<ChessPosition> possibleMoves = new Vector<>();
+        for(int r=myRow-1; r<=myRow+1; r++){
+            for(int c=myCol-1; c<=myCol+1; c++){
+                possibleMoves.add(new ChessPosition(r,c));
+            }
+        }
+
+        //Determines if possibleMoves belong in kingMoves
+        while(!possibleMoves.isEmpty()){
+            ChessPosition pos = possibleMoves.firstElement();
+            //safety check to remove current position from possibleMoves
+            if(pos == myPosition){possibleMoves.remove(0);}
+
+            //safety check to ensure pos is on the board
+            if (!isOnBoard(pos)) {possibleMoves.remove(0);}
+
+            //if pos is clear or populated by the enemy, adds it to kingMoves
+            else if (isClear(board, pos)) {
+                kingMoves.add(new ChessMove(myPosition,pos,null));
+                possibleMoves.remove(0);
+            } else if (board.getPiece(pos).getTeamColor()!=myColor) {
+                kingMoves.add(new ChessMove(myPosition,pos,null));
+                possibleMoves.remove(0);
+            }
+            else{
+                possibleMoves.remove(0);
+            }
+        }
+        
         return kingMoves;
+    }
+
+    private boolean isClear(ChessBoard board, ChessPosition position){
+        return (board.getPiece(position)==null);
+    }
+    //returns true if row and column of position are between 1 and 8
+    private boolean isOnBoard(ChessPosition position){
+        return 1 <= position.getRow() && position.getRow() <= 8 &&
+                1 <= position.getColumn() && position.getColumn() <= 8;
     }
 
     @Override
