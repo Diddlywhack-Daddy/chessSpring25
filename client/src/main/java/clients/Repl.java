31a2eb220dba38953b4d710d6a419e321f80e6e6
Jacob Larson <jl2379@byu.edu>
2025-06-07 20:09:53 -0400

    Finished Repl Class

--- client/src/main/java/clients/Repl.java
+++ client/src/main/java/clients/Repl.java
@@ -8,7 +8,6 @@ public class Repl {
     private final PostLoginClient postLoginClient;
 
     public Repl(String serverURL) {
-
         this.gameClient = new GameClient(serverURL);
         this.preLoginClient = new PreLoginClient(serverURL);
         this.postLoginClient = new PostLoginClient(serverURL);
@@ -16,63 +15,74 @@ public class Repl {
 
     public void run() {
         System.out.println("Welcome to chess.");
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         preLoginRepl(scanner);
-
     }
 
-
-    //This will be my first layer, the prelogin repl
-    private void preLoginRepl(Scanner scanner){
-        var result="";
+    // This is the first layer, the pre-login REPL
+    private void preLoginRepl(Scanner scanner) {
+        var result = "";
         System.out.print(preLoginClient.help());
+        printPrompt();
         while (!result.equals("quit")) {
             String line = scanner.nextLine();
-
-
             try {
                 result = preLoginClient.eval(line);
-                if(result.equals("postLogin")){
+                if (result.equals("postLogin")) {
                     postLoginRepl(scanner);
                     System.out.print(preLoginClient.help());
+                } else {
+                    System.out.print(result);
                 }
+                printPrompt();
             } catch (Throwable e) {
-                var msg = e.toString();
-                System.out.print(msg);
+                System.out.print(e.toString());
+                printPrompt();
             }
         }
         System.out.println();
     }
 
-    //This will be my second layer, postlogin repl. It is called by preLogin and uses break in the quit
-    private void postLoginRepl(Scanner scanner){
-        var result="";
+    // This is the second layer, the post-login REPL
+    private void postLoginRepl(Scanner scanner) {
+        var result = "";
         System.out.print(postLoginClient.help());
+        printPrompt();
         while (!result.equals("loggedOut")) {
             String line = scanner.nextLine();
-
-
             try {
                 result = postLoginClient.eval(line);
-                System.out.print(result);
-                if(result == "login"){
-                    postLoginRepl(scanner);
+                if (result.equals("play") || result.equals("observe")) {
+                    gameplayRepl(scanner);
                     System.out.print(postLoginClient.help());
+                } else {
+                    System.out.print(result);
                 }
+                printPrompt();
             } catch (Throwable e) {
-                var msg = e.toString();
-                System.out.print(msg);
+                System.out.print(e.toString());
+                printPrompt();
             }
         }
         System.out.println();
     }
 
-    //This is the last layer, the gameplay repl. It is called by postLogin and uses break in the quit
-    private void gameplayRepl(){
-
+    // This is the final layer, the gameplay REPL
+    private void gameplayRepl(Scanner scanner) {
+        // Placeholder for in-game interaction (move input, resign, redraw board, etc.)
+        System.out.println("Entered gameplay mode. Type 'resign' or 'exit' to leave game mode.");
+        printPrompt();
+        String command = "";
+        while (!command.equals("resign") && !command.equals("exit")) {
+            command = scanner.nextLine().trim().toLowerCase();
+            // TODO: Add actual gameplay command handling here
+            System.out.println("You entered: " + command);
+            printPrompt();
+        }
+        System.out.println("Exited gameplay mode.");
     }
 
     private void printPrompt() {
-        System.out.print("\n" + ">>> " );
+        System.out.print("\n>>> ");
     }
 }
